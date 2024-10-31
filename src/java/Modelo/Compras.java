d1ec7cd626e7a4d1ffb609c9520068683fc8f8e0
Milton Cuadra <128653541+Milthor21@users.noreply.github.com>
2024-10-30 20:12:43 -0600

    Se crearon el modelo de Compras

---
+++ src/java/Modelo/Compras.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author macma
+ */
+public class Compras {
+    
+    int idCompra;
+    String fecha;
+    float montoTotal;
+    int idCliente;
+    int idContrato;
+
+    public Compras(int idCompra, String fecha, float montoTotal, int idCliente, int idContrato) {
+        this.idCompra = idCompra;
+        this.fecha = fecha;
+        this.montoTotal = montoTotal;
+        this.idCliente = idCliente;
+        this.idContrato = idContrato;
+    }
+
+    public Compras(){
+    }
+    
+    public int getIdCompra() {
+        return idCompra;
+    }
+
+    public void setIdCompra(int idCompra) {
+        this.idCompra = idCompra;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public float getMontoTotal() {
+        return montoTotal;
+    }
+
+    public void setMontoTotal(float montoTotal) {
+        this.montoTotal = montoTotal;
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public int getIdContrato() {
+        return idContrato;
+    }
+
+    public void setIdContrato(int idContrato) {
+        this.idContrato = idContrato;
+    }
+    
+    
+}
